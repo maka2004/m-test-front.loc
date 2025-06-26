efb3050aa1ff8a2b40303602ea89328c397cd5a2
maksym.samoilovych <maksym.samoilovych@akurateco.com>
2025-06-26 10:54:14 +0300

    fix logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router } from '@angular/router';
 import { AuthService } from './auth/auth';
 
 @Component({
@@ -10,5 +10,10 @@ import { AuthService } from './auth/auth';
   styleUrl: './app.scss'
 })
 export class App {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
